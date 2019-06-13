8a8484777a81dead29d914d30756c63cf070ff2d
miyata710 <39073850+miyata710@users.noreply.github.com>
2019-06-13 14:41:19 -0400

    Create findOperatingVoltage-NULL.py

---
+++ findOperatingVoltage-NULL.py
@@ -0,0 +1,51 @@
+'''
+https://gis.stackexchange.com/questions/230536/adding-values-from-list-to-field-in-feature-class-using-arcpy
+'''
+import arcpy
+
+arcpy.env.workspace = r'E:\Data\EROlson\test.gdb'
+
+#input data
+transformer = 'Transformer'
+
+priOH = 'test_OH_pri'
+
+priUG = 'PriUG'
+
+misc = 'MiscNetFeat'
+
+fuse = 'Fuse'
+
+isolator = 'Transformer'
+
+dataList = [transformer, priOH, priUG, misc, fuse]
+fields = ["FEEDERID"]
+allFeederID = []
+nullFeederID = []
+isolatorFeederID = []
+  
+for i in dataList:
+    delim = arcpy.AddFieldDelimiters(i,"OPERATINGVOLTAGE") + ' IS NULL'
+    cursor = arcpy.da.SearchCursor(i,fields,delim)
+    for row in cursor:
+        #check to see if feeder ID associated with isolator
+
+        #if associated with isolator:
+        #put in different list from nulls
+        if row[0] not in allFeederID:
+            allFeederID.append(row[0])
+
+del cursor
+
+for feeder in allFeederID:
+    cursor = arcpy.da.SearchCursor(isolator,"*","SUBTYPECD = 10 AND FEEDERID = '{}'".format(feeder))
+    for row in cursor:
+        isolatorFeederID.append(row[0])
+    else:
+        nullFeederID.append(feeder)
+
+del cursor
+
+
+
+
