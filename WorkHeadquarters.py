0bf43792ac82ba53386c7a706af61a584200477b
miyata710 <39073850+miyata710@users.noreply.github.com>
2019-06-17 09:59:21 -0400

    Update WorkHeadquarters.py

--- WorkHeadquarters.py
+++ WorkHeadquarters.py
@@ -1,1 +1,15 @@
+'''
+This script is being developed for the purpose of updating the work headquarters field for the following feature classes:
+                    1.	Dynamic Protective Device
+                    2.	Fuse
+                    3.	Miscellaneous Network Features (MNF) 
+                    4.	PF Correcting Equipment (Capacitor in ArcMap TOC)
+                    5.	Primary Overhead Conductor
+                    6.	Primary Underground Conductor
+                    7.	Secondary Overhead Conductor
+                    8.	Secondary Underground Conductor
+                    9.	Switch
+                    10.	Transformer
+                    11.	Voltage Regulator (Regulators and Boosters in ArcMap TOC)
 
+'''
