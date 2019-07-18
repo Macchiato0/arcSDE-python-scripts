b94fa8fe9bef7905eef0662e92091bad70416870
miyata710 <39073850+miyata710@users.noreply.github.com>
2019-07-18 11:33:28 -0400

    Update updateOperatingVoltage.py

--- updateOperatingVoltage.py
+++ updateOperatingVoltage.py
@@ -2,4 +2,5 @@
 Need to update known operating voltage for substations with NO isolators for every required feature class. 
 
 -can possibly use circuit source table to find operating voltage for sub DGSEP011.sde ELECTDIST.CircuitSource
+r'Database Connections\Connection to DGSEP011.sde\ELECDIST.CircuitSource'
 '''
