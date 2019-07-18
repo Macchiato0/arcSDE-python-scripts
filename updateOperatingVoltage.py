64e1792b2e6b1d0d6bec80734a333117902a0a5f
miyata710 <39073850+miyata710@users.noreply.github.com>
2019-07-18 10:07:27 -0400

    Update updateOperatingVoltage.py

--- updateOperatingVoltage.py
+++ updateOperatingVoltage.py
@@ -1,3 +1,5 @@
 '''
 Need to update known operating voltage for substations with NO isolators for every required feature class. 
+
+-can possibly use circuit source table to find operating voltage for sub
 '''
