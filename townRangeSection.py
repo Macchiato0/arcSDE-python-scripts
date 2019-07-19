1b35be3ce06949356f52cafe489f04af0ab90f77
miyata710 <39073850+miyata710@users.noreply.github.com>
2019-07-19 14:09:16 -0400

    Update townRangeSection.py

--- townRangeSection.py
+++ townRangeSection.py
@@ -1,41 +1,13 @@
-'''
-Need script to update TRS
-
--need landbase sde connection for spatial selection (brock sent in email)
--section name FC has section name attribute field. needs to be moved to...misc. net feature,fuse,switch (will be done by feederID) 
-to town range section (string value) in provided FCs
-
-'''
-
 def calculateTownRangeSection():
-  updateFields = "TRS"
-  feederField = "FEEDERID"
-  trsSearchField = "SECTIONNAME"
-  for feeder in feederID:
+  ####Some basic steps####
+  '''
+  SectionName is a feature class in the LandBase SDE connection. There is a 6-digit numeric sequence that is a unique ID for each polygon
+  in the sectionname attribut field.
+  
+  We need the section name code transfered for the following feature classes for a specific feederID:
+    -Misc. Network Features
+    -Fuse
+    -Switch
     
-    SQL = """{0} = '{1}' AND {2} IS NULL""".format(arcpy.AddFieldDelimiters(dataPath,feederField),feeder,arcpy.AddFieldDelimiters(dataPath,workHQField))
-
-    #set workspace
-    workspace = r'E:\Data\EROlson\PROD_ DGSEP011AsEROlson.sde'
-
-    # Start an edit session. Must provide the worksapce.
-    edit = arcpy.da.Editor(workspace)
-
-    # Edit session is started without an undo/redo stack for versioned data
-    #  (for second argument, use False for unversioned data)
-    edit.startEditing(False, True)
-
-    # Start an edit operation
-    edit.startOperation()
-
-    updateCursor = arcpy.da.UpdateCursor(dataPath,updateFields,SQL)
-    for row in updateCursor:
-        row[0] = workHeadquarters
-        updateCursor.updateRow(row)
-    del updateCursor
-
-    # Stop the edit operation.
-    edit.stopOperation()
-
-    # Stop the edit session and save the changes
-    edit.stopEditing(True)
+  Some sort of "select by location" analysis will need to be done to identify which feederIDs are within a given section.
+  '''
