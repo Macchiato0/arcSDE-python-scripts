28d1a4a5053dfdd654a2e78a962d74e7d17a2943
miyata710 <39073850+miyata710@users.noreply.github.com>
2019-07-19 13:55:07 -0400

    Update townRangeSection.py

--- townRangeSection.py
+++ townRangeSection.py
@@ -6,3 +6,36 @@ Need script to update TRS
 to town range section (string value) in provided FCs
 
 '''
+
+def calculateTownRangeSection():
+  updateFields = "TRS"
+  feederField = "FEEDERID"
+  trsSearchField = "SECTIONNAME"
+  for feeder in feederID:
+    
+    SQL = """{0} = '{1}' AND {2} IS NULL""".format(arcpy.AddFieldDelimiters(dataPath,feederField),feeder,arcpy.AddFieldDelimiters(dataPath,workHQField))
+
+    #set workspace
+    workspace = r'E:\Data\EROlson\PROD_ DGSEP011AsEROlson.sde'
+
+    # Start an edit session. Must provide the worksapce.
+    edit = arcpy.da.Editor(workspace)
+
+    # Edit session is started without an undo/redo stack for versioned data
+    #  (for second argument, use False for unversioned data)
+    edit.startEditing(False, True)
+
+    # Start an edit operation
+    edit.startOperation()
+
+    updateCursor = arcpy.da.UpdateCursor(dataPath,updateFields,SQL)
+    for row in updateCursor:
+        row[0] = workHeadquarters
+        updateCursor.updateRow(row)
+    del updateCursor
+
+    # Stop the edit operation.
+    edit.stopOperation()
+
+    # Stop the edit session and save the changes
+    edit.stopEditing(True)
