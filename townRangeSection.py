c7a9022ebf97bc7799a08cbebc13ffb847dfe721
miyata710 <39073850+miyata710@users.noreply.github.com>
2019-07-19 13:38:08 -0400

    Update townRangeSection.py

--- townRangeSection.py
+++ townRangeSection.py
@@ -2,7 +2,7 @@
 Need script to update TRS
 
 -need landbase sde connection for spatial selection (brock sent in email)
--section name (from which FC???) needs to be moved to...misc. net feature,fuse,switch (will be done by feederID) to town range section (string value) 
-in provided FCs
+-section name FC has section name attribute field. needs to be moved to...misc. net feature,fuse,switch (will be done by feederID) 
+to town range section (string value) in provided FCs
 
 '''
