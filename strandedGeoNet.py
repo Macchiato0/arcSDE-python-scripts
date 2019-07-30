374fc19f2801ea9037b91903bf41b84ea6b57dda
miyata710 <39073850+miyata710@users.noreply.github.com>
2019-07-30 14:43:12 -0400

    Update strandedGeoNet.py

--- strandedGeoNet.py
+++ strandedGeoNet.py
@@ -1,7 +1,37 @@
+#Arcmap settings
+arcpy.env.overwriteOutput = True
+arcpy.env.addOutputsToMap = False
+
+###input data paths###
+
+#OH Connector Line
+OHline = r'Customers & Transformers\Transformer Connector Lines\OH Connector Line'
+
+#UG Connector Line
+UGline = r'Customers & Transformers\Transformer Connector Lines\UG Connector Line'
+
+#Primary Overhead dataset
+priOH = r'Primary Lines\Primary Overhead Conductor'
+
+#Primary Underground dataset
+priUG = r'Primary Lines\Primary Underground Conductor'
+
+#Secondary Overhead dataset
+secOH = r'Customers & Transformers\Secondary Overhead Conductor'
+
+#Secondary Underground dataset
+secUG = r'Customers & Transformers\Secondary Underground Conductor'
+
+#Geometric Network junctions dataset
+geoNetJunct = r'Misc Network Features\ELECDIST.ElectricGeomNetwork_Junctions'
+
 ###Find geometric network junctions attached to lines --> delete all others###
 
 #list of geometric network junctions that are intersected by a layer in selectByList
 geoNetJunctIntersectedList = []
+
+#list of feature classes used in select by location analysis to find intersected geometric network junctions
+selectByList = [OHline, UGline, priOH, priUG, secOH, secUG]
 
 #Start looping through FCs
 
