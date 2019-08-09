d964e076d413e2bfe763a6967e4a5eb9c50d37c4
miyata710 <39073850+miyata710@users.noreply.github.com>
2019-08-09 10:30:39 -0400

    Update WorkHeadquarters.py

--- WorkHeadquarters.py
+++ WorkHeadquarters.py
@@ -50,15 +50,7 @@ def calculateHQ(feederID,dataPath,workHeadquarters):
         
 #### Function Parameters ####
 feederID = [
-'029601',
-'029602',
-'078601',
-'078602',
-'077201',
-'077202',
-'077203',
-'026501',
-'026502'
+
 ]
 workHeadquarters = 'HML'
 priOH = r'Primary Lines\Primary Overhead Conductor'
